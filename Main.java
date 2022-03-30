18de4e3bc3d7724d9f501d6829b0427085338dea
Zhenya <evgen21111993@yandex.ru>
2022-03-30 21:31:09 +0300

    add parametrs

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    private int sumHotWater = 0; //м,куб
+    private int sumColdWater = 0; //м,куб
+
+    private static int limitHotWater = 10; //м,куб
+    private static int limitColdWater = 15; //м,куб
+
+    private static int priceLimitHotWater = 4; //руб.
+    private static int priceHotWater = 6; //руб.
+    private static int priceLimitColdWater = 2; //руб.
+    private static double priceColdWater = 3; //руб.
+
+    private double totalPay = 0; //руб.
+
+
+}
